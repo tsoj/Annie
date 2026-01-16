c67d0f3e1800dde9dd2a2af8b386c9098ce12ae2
Jost Triller <tsoj.tsoj@gmx.de>
2026-01-16 21:39:42 +0100

    version increase for no reason

--- version.nim
+++ version.nim
@@ -1,5 +1,5 @@
 import strutils
 
-func version*(): string = "1.1"
+func version*(): string = "1.2"
 func compileDate*(): string = CompileDate & " " & CompileTime & " (UTC)"
 func compileYear*(): string = CompileDate.split('-')[0]
